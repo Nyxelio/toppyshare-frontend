f48b00f0760fdc9579f4b4c0cd3fa00f3afd3ba0
Merridia <jennifer.droza@y-nov.com>
2015-05-18 18:15:41 +0200

    modif index

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -6,3 +6,21 @@ angular.module("app", ["chart.js"]).controller("RadarCtrl", function ($scope) {
     [28, 48, 40, 19, 96, 27, 100]
   ];
 });
+
+function showPassword() {
+    
+    var key_attr = $('#key').attr('type');
+    
+    if(key_attr != 'text') {
+        
+        $('.checkbox').addClass('show');
+        $('#key').attr('type', 'text');
+        
+    } else {
+        
+        $('.checkbox').removeClass('show');
+        $('#key').attr('type', 'password');
+        
+    }
+    
+}
