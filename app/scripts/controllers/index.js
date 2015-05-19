fd665c0b8665f4ff1e96a3525a73a43a1561c7bd
Merridia <jennifer.droza@y-nov.com>
2015-05-19 17:37:05 +0200

    Change views architecture

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -1,10 +1,17 @@
-angular.module("app", ["chart.js"]).controller("RadarCtrl", function ($scope) {
-  $scope.labels =["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"];
+var app = angular.module("app", ["chart.js"]);
+
+app.controller("RadarCtrl", function ($scope) {
+  $scope.labels = ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"];
 
   $scope.data = [
     [65, 59, 90, 81, 56, 55, 40],
     [28, 48, 40, 19, 96, 27, 100]
   ];
+});
+
+app.controller("ApplicationCtrl", function($scope){
+  console.log('coucou');
+  $scope.loginOpened  = false;
 });
 
 function showPassword() {
